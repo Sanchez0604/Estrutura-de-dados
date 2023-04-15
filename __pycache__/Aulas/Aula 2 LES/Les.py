7b07c664b3910c3a5955b49e1537cb110fa88cb0
Sanchez0604 <jpedro.c@outlook.com>
2023-04-15 09:16:00 -0300

    verificação de lista cheia/vazia

--- __pycache__/Aulas/Aula 2 LES/Les.py
+++ __pycache__/Aulas/Aula 2 LES/Les.py
@@ -4,9 +4,17 @@ class Les:
         self.vetor=[None] * self.tam_maximo
         self.quant=0
     def inserir_fim(self,valor):
-        self.vetor[self.quant]=valor
-        self.quant+=1
+        if self.quant==self.tam_maximo:
+            print('A lista está cheia!')
+        else:
+            self.vetor[self.quant]=valor
+            self.quant+=1
     def show(self):
         for i in range(self.quant):
             print(self.vetor[i], end=" ")
         print()
+    def remover_fim():
+        if self.quant==self.tam_maximo:
+            print('A lista está vazia')
+        else:
+            self.quant-=1
