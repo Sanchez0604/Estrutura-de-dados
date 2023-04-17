18eb2d98cfa43cf887464f090a624a41d78a8238
Sanchez0604 <jpedro.c@outlook.com>
2023-04-17 16:34:02 -0300

    Lista estatica sequencial

--- __pycache__/Aulas/Aula 2 LES/Les.py
+++ __pycache__/Aulas/Aula 2 LES/Les.py
@@ -1,3 +1,6 @@
+from typing import Self
+
+
 class Les:
     def __init__(self,tamanho):
         self.tam_maximo=tamanho
@@ -18,3 +21,52 @@ class Les:
             print('A lista está vazia')
         else:
             self.quant-=1
+    def inserir_inicio(self,valor):
+        for i in range(self.quant,0,-1):
+            self.vetor[i]=self.vetor[i-1]
+        self.vetor[0]=valor
+        self.quant+=1
+    def remover_inicio(self):
+        for i in range(self.quant-1):
+            self.vetor[i] = self.vetor[i+1]
+        self.quant -=1
+    def remover_elemento(self,valor):
+        for i in range(self.tam_maximo):
+            if self.vetor[i]== valor:
+                for value in range(i,self.quant):
+                    self.vetor[value]=self.vetor[value+1]
+        self.quant-=1
+    def buscar(self,valor):
+        for i in range(self.quant):
+            if self.vetor[i]== valor:
+                return "encontrado"
+            else:
+                return "não encontrado"
+            
+    def lista_cheia(self):
+        if self.quant == self.tam_maximo:
+            return True
+        else:
+            return False
+
+    def inserir_apos(self,valor1,valor2):
+        if self.lista_cheia() == False:
+            for i in range(self.quant):
+                if self.vetor[i]== valor2:
+                    for value in range(self.quant,i,-1):
+                        self.vetor[value]=self.vetor[value-1]
+                    self.vetor[i+1]=valor1
+            self.quant+=1
+    def inserir_antes(self,valor1,valor2):
+        if self.lista_cheia() == False:
+            for i in range(self.quant):
+                if self.vetor[i]== valor2:
+                    for value in range(self.quant,i,-1):
+                        print(value)
+                        self.vetor[value]=self.vetor[value-1]
+                    self.vetor[i-1]=valor1
+                    return
+                
+            
+            
+                    
--- __pycache__/Aulas/Aula 2 LES/usaLes.py
+++ __pycache__/Aulas/Aula 2 LES/usaLes.py
@@ -4,10 +4,6 @@ import Les
 # igual a 5
 lista = Les.Les(5)
 
-# Insere o elemento 1 na lista
-lista.inserir_fim(1)
-lista.show()
-
 # Insere o elemento 3 na lista
 lista.inserir_fim(3)
 lista.show()
@@ -23,3 +19,18 @@ lista.show()
 # Insere o elemento 9 na lista
 lista.inserir_fim(9)
 lista.show()
+
+# Remove o elemento 7 da lista e realoca os elementos a partir da posição do 7
+lista.remover_elemento(7)
+lista.show()
+
+# Busca o elemento e retorna o indice
+lista.buscar(3)
+
+# Adiciona o elemento após o elemento dado
+#lista.inserir_apos(6,5)
+#lista.show()
+
+# Adiciona o elemento antes do elemento dado
+lista.inserir_antes(7,9)
+lista.show
