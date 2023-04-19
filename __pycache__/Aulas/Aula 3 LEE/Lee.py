e7ab15ed1c89758d36c7c9f1142bd6c1573527a4
Sanchez0604 <jpedro.c@outlook.com>
2023-04-19 14:31:33 -0300

    Lista estatica Encadeada

--- __pycache__/Aulas/Aula 3 LEE/Lee.py
+++ __pycache__/Aulas/Aula 3 LEE/Lee.py
@@ -40,7 +40,40 @@ class Lee:
         print('Vetor=')
         for i in range(self.tam_maximo):
             print(i, self.vetor[i].info, self.vetor[i].prox)
+    def inserir_fim(self,valor):
+        if self.quant == 0:
+            self.prim = self.ult = self.ocupa_no(valor,-1)
+            self.quant += 1
+        else:
+            self.vetor[self.ult].prox = self.ult = self.ocupa_no(valor,-1)
+            self.quant += 1
+    def devolve_no(self, no):
+        self.vetor[no].prox = self.primeira_posicao_vazia
+        self.primeira_posicao_vazia = no
+    def remover_inicio(self):
+        if self.quant == 1:
+            self.devolve_no(self.prim)
+            self.prim = self.ult = -1
+        else:
+            novo_prim = self.vetor[self.prim].prox
+            self.devolve_no(self.prim)
+            self.prim = novo_prim
+        self.quant-=1
+    def remover_fim(self):
+        if self.quant == 1: #Verifica se a lista só possui um elemento
+            self.devolve_no(self.prim) # se for verdadeiro, a função devolve_no recebe o endereço do primeiro elemento
+            self.prim = self.ult =-1 # self.prim e self.ult recebem -1 indicando que a lista está vazia
+        else:
+            aux=self.prim #Recebe o primeiro elemento da lista
+            while self.vetor[aux].prox != self.ult: #Enquanto o self.vetor[aux].prox for diferente do ultimo nó
+                aux = self.vetor[aux].prox #Aux recebe o endereço do proximo nó
+            self.devolve_no(self.ult)  # Fora do laço while, a função devolve nó recebe como paramentro o ultimo elemento da lista
+            self.vetor[aux].prox = -1 # O Atributo prox de self.vetor[aux].prox recebe -1 indicando ser o ultimo elemento da lista
+            self.ult = aux # sel.ult recebe aux como ultimo elemento
+        self.quant -=1 #self.quant é decrementado em 1 elemento
 
+
+        
         '''def inicializa_estrutura(self):
             for i in range(self.tam_maximo-1):
                 self.vetor[i] = No(None,i+1)
